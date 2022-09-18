5b5caf6e862c88e79e6e945262648300783dee63
mateuszxstepien <mateuszxstepien@gmail.com>
2022-09-18 10:45:39 +0200

    ver. 1.0

---
+++ js/slicksettings.js
@@ -0,0 +1,29 @@
+$('.team-carousel').slick(
+    
+    {
+    arrows: false,
+    autoplay: true,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive:[{
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3
+            }
+        }
+    ]
+  }
+  );
